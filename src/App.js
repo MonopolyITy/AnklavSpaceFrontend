4e8fec9ef8e3b689796483a3da00420ff240b06c
Pacman <pacman@MacBook-Pro-Pacman.local>
2025-10-14 13:48:29 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { useEffect } from 'react';
+import Home from './pages/Home/Home';
+import Header from './components/Header/Header';
 
 const tg = window.Telegram.WebApp;
 
 const App = () => {
   
-  useEffect(() => {
-     const isMobileFullscreen = /Android|iPhone|iPad/i.test(navigator.userAgent);
-  
+  useEffect(() => {  
       tg.ready();
       tg.expand();
+
+      //  const isMobileFullscreen = /Android|iPhone|iPad/i.test(navigator.userAgent);
+      // if (isMobileFullscreen) {
+      //   tg.requestFullscreen();
+      // }
     
-      if (isMobileFullscreen) {
-        tg.requestFullscreen();
-      }
-    
-      tg.setHeaderColor('#FFFFFF');
+      tg.setHeaderColor('#4E4C50');
   }, []);
 
   return (
-    <div style={{height: "100vh", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center"}}>
-      <p>Anklav Space</p>
-    </div>
+    <>
+      <Header/>
+      <Home/>
+    </>
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Header.css"
+import { ReactComponent as Logo } from './logo.svg';
+
+const Header = () => {
+  return (
+    <div style={{background: "#4E4C50", color: "white", padding: "20px", display: "flex", alignItems: "center"}}>
+        <Logo style={{marginTop: "4px"}}/>
+        <div style={{marginLeft: "15px"}}>
+            <p className='font-display' style={{fontSize: "14px"}}>ANKLAV SPACE</p>
+            <p style={{fontSize: "11px", opacity: "0.8"}}>Укрепите бизнес через ясность в ролях,<br/>долях и ответственности</p>
+        </div>
+    </div>
+  )
+}
+
+export default Header

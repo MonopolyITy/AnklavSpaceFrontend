7f7174a798addd6f97c7b1382f8f31c497981b1f
Pacman <pacman@MacBook-Pro-Pacman.local>
2025-10-15 15:38:55 +0300

    commit

--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 import { useParams, useSearchParams } from "react-router-dom";
-import axios from '../../api/axios';
+// import axios from '../../api/axios';
 // import text from './text.js'
 
 
@@ -11,19 +11,19 @@ const Quiz = () => {
     const [searchParams] = useSearchParams();
     const name = searchParams.get("name") || "Guest";
 
-    const [room, setRoom] = useState(null);
+//     const [room, setRoom] = useState(null);
 
-    useEffect(() => {
-        axios.get(`/api/rooms/${roomId}`)
-        .then(response => {
-            setRoom(response.data);
-        })
-        .catch(error => {
-            console.error('Ошибка при загрузке комнаты:', error);
-        });
-  }, [roomId]);
+//     useEffect(() => {
+//         axios.get(`/api/rooms/${roomId}`)
+//         .then(response => {
+//             setRoom(response.data);
+//         })
+//         .catch(error => {
+//             console.error('Ошибка при загрузке комнаты:', error);
+//         });
+//   }, [roomId]);
 
-  console.log(room.members, name)
+//   console.log(room.members, name)
 
   return (
     <div>
